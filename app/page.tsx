6d94925d94c801a93f51d1a68cc28511b4ffd10f
Kang-Seonggyu <103415733+Kang-Seonggyu@users.noreply.github.com>
2024-07-25 17:26:43 +0900

    Feat: Visual Text

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,61 @@
+'use client';
+
+import { useEffect, useRef, useState } from 'react';
 import st from './page.module.css';
-import dynamic from 'next/dynamic';
+import AnimatedRender from '@/components/common/animated-render';
 import LidButton from '@/components/common/lid-button';
+import ScrollDown from '@/components/common/scroll-down';
+import { Audiowide } from 'next/font/google';
+
+const audiowide = Audiowide({
+  subsets: ['latin'],
+  weight: '400',
+});
 
 export default function Home() {
-  const LazyLoading = dynamic(
-    () => import('@/components/common/animated-render'),
-    {
-      loading: () => <p>Loading...</p>,
-    }
-  );
+  const [maxHeight, setMaxHeight] = useState(1);
+  const [scrollY, setScrollY] = useState(0);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    const handleResize = () => {
+      setMaxHeight(window.innerHeight);
+    };
+    handleResize();
+
+    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const scrollPercentage = Math.min(scrollY / (maxHeight - 100), 1);
+  const moveX = Math.round(scrollPercentage * 100) * 2;
 
   return (
-    <main className={st.main}>
+    <main ref={ref} className={st.main}>
       <div className={st.visual}>
         <div id={st.stars}></div>
         <div id={st.stars2}></div>
         <div id={st.stars3}></div>
 
-        <div className={st.name}>
-          <p>kang seong gyu</p>
-          <p>frontend</p>
+        <div className={`${st.visual_text} ${audiowide.className}`}>
+          <p style={{ transform: `translateX(${moveX}vw)` }}>kang seong gyu</p>
+          <p
+            style={{ transform: `translateX(-${moveX}vw)`, textAlign: 'right' }}
+          >
+            frontend
+          </p>
         </div>
       </div>
+      <ScrollDown style={{ top: 'calc(-10vh - 50px)' }}></ScrollDown>
 
       <div className={st.contents}>
         <div className="spacer" style={{ width: '100%', height: '30vh' }}>
@@ -37,16 +71,16 @@ export default function Home() {
             color="blue"
           ></LidButton>
         </div>
-        <LazyLoading>
+        <AnimatedRender>
           <div style={{ width: '300px', height: '200px', background: 'green' }}>
             애니메이션 렌더러
           </div>
-        </LazyLoading>
-        <LazyLoading>
-          <div style={{ width: '200px', height: '400px', background: 'red' }}>
+        </AnimatedRender>
+        <AnimatedRender>
+          <div style={{ width: '300px', height: '200px', background: 'red' }}>
             애니메이션 렌더러
           </div>
-        </LazyLoading>
+        </AnimatedRender>
         <div className="spacer" style={{ width: '100%', height: '90vh' }}></div>
       </div>
     </main>
