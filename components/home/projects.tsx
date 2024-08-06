12f144c3ee2016ff07a408be0622c5b4605e729c
Kang-Seonggyu <103415733+Kang-Seonggyu@users.noreply.github.com>
2024-08-06 13:20:14 +0900

    Feat: 프로젝트(경주정보고1차)

--- components/home/projects.tsx
+++ components/home/projects.tsx
@@ -4,6 +4,10 @@ import st from './projects.module.css';
 export function Projects() {
   // prettier-ignore
   const projects = {
+    'temp_jobportal':{
+      skills:['React','Redux','Redux-saga'],
+      images: [{src:'/images/projects/jobportal/jobportal1.png',detail:''},{src:'/images/projects/jobportal/jobportal2.png',detail:''},{src:'/images/projects/jobportal/jobportal3.png',detail:''},{src:'/images/projects/jobportal/jobportal4.png',detail:''},{src:'/images/projects/jobportal/jobportal5.png',detail:''},{src:'/images/projects/jobportal/jobportal6.png',detail:''}]
+    },
     'P.Front': {
       skills:['React','TailwindCSS','Redux','Redux-saga','NodeJs'],
       images: [{src:'/images/projects/pfront/pfront1.png',detail:''},{src:'/images/projects/pfront/pfront2.png',detail:''},{src:'/images/projects/pfront/pfront3.png',detail:''},{src:'/images/projects/pfront/pfront4.png',detail:''},{src:'/images/projects/pfront/pfront5.png',detail:''},{src:'/images/projects/pfront/pfront6.png',detail:''},{src:'/images/projects/pfront/pfront7.png',detail:''}]
@@ -13,7 +17,6 @@ export function Projects() {
   return (
     <section>
       <h2>Projects</h2>
-
       <div className={st.container}>
         <h3>P.Front (포항공대복지회)</h3>
         <div className={st.content}>
@@ -29,7 +32,7 @@ export function Projects() {
             </div>
             <div>
               <label>Period</label>
-              <span>2022.11~2023.05</span>
+              <span>2023.02 ~ 2023.08</span>
             </div>
             <div>
               <label>Desc</label>
@@ -49,6 +52,45 @@ export function Projects() {
                 <p>데이터 엑셀 다운로드 기능 구현</p>
                 <p>통계 이력에 대한 데이터 시각화</p>
                 <p>주문 내역 데이터 시각화 및 환불기능</p>
+              </div>
+            </div>
+          </desc>
+        </div>
+      </div>
+
+      <div className={st.container}>
+        <h3>경주정보고 취업포털(1차)</h3>
+        <div className={st.content}>
+          <Carousel data={projects['temp_jobportal'].images}></Carousel>
+          <desc>
+            <div>
+              <label>Skills</label>
+              <span className={st.flexwrap}>
+                {projects['temp_jobportal'].skills.map((skill, i) => (
+                  <em key={`temp_jobportal_${skill}_${i}`}>{skill}</em>
+                ))}
+              </span>
+            </div>
+            <div>
+              <label>Period</label>
+              <span>2022.11 ~ 2023.05</span>
+            </div>
+            <div>
+              <label>Desc</label>
+              <span>
+                <b>포항공대 내부 매장들을 관리를 하는 웹사이트</b>입니다.
+              </span>
+            </div>
+            <div>
+              <label>Detail</label>
+              <div>
+                <p>사람인/잡코리아 취업정보 API 연동</p>
+                <p>필터링 조건 추가 및 삭제 기능 구현</p>
+                <p>Redux를 이용한 상태관리</p>
+                <p>Redux-saga를 이용한 API 연동</p>
+                <p>관리자 페이지(회원관리)</p>
+                <p>교직원 페이지(관할학생정보관리)</p>
+                <p>취업 통계 데이터 시각화</p>
               </div>
             </div>
           </desc>
