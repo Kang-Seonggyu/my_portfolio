4d90e007a3474960bf67539f4ba63f97db583787
Kang-Seonggyu <103415733+Kang-Seonggyu@users.noreply.github.com>
2024-08-06 14:36:01 +0900

    Feat: 프로젝트(경주정보고2차)

--- components/home/projects.tsx
+++ components/home/projects.tsx
@@ -8,6 +8,10 @@ export function Projects() {
       skills:['React','Redux','Redux-saga'],
       images: [{src:'/images/projects/jobportal/jobportal1.png',detail:''},{src:'/images/projects/jobportal/jobportal2.png',detail:''},{src:'/images/projects/jobportal/jobportal3.png',detail:''},{src:'/images/projects/jobportal/jobportal4.png',detail:''},{src:'/images/projects/jobportal/jobportal5.png',detail:''},{src:'/images/projects/jobportal/jobportal6.png',detail:''}]
     },
+    'jobportal':{
+      skills:['JQuery','JQueryUI'],
+      images: [{src:'/images/projects/jobportal2/경주정보고1.png',detail:''},{src:'/images/projects/jobportal2/경주정보고2.png',detail:''},{src:'/images/projects/jobportal2/경주정보고3.png',detail:''},{src:'/images/projects/jobportal2/경주정보고4.png',detail:''},{src:'/images/projects/jobportal2/경주정보고5.png',detail:''},{src:'/images/projects/jobportal2/경주정보고6.png',detail:''}]
+    },
     'P.Front': {
       skills:['React','TailwindCSS','Redux','Redux-saga','NodeJs'],
       images: [{src:'/images/projects/pfront/pfront1.png',detail:''},{src:'/images/projects/pfront/pfront2.png',detail:''},{src:'/images/projects/pfront/pfront3.png',detail:''},{src:'/images/projects/pfront/pfront4.png',detail:''},{src:'/images/projects/pfront/pfront5.png',detail:''},{src:'/images/projects/pfront/pfront6.png',detail:''},{src:'/images/projects/pfront/pfront7.png',detail:''}]
@@ -78,7 +82,7 @@ export function Projects() {
             <div>
               <label>Desc</label>
               <span>
-                <b>포항공대 내부 매장들을 관리를 하는 웹사이트</b>입니다.
+                <b>경주정보고 학생들의 취업을 돕기 위한 웹사이트</b>입니다.
               </span>
             </div>
             <div>
@@ -91,6 +95,51 @@ export function Projects() {
                 <p>관리자 페이지(회원관리)</p>
                 <p>교직원 페이지(관할학생정보관리)</p>
                 <p>취업 통계 데이터 시각화</p>
+              </div>
+            </div>
+          </desc>
+        </div>
+      </div>
+
+      <div className={st.container}>
+        <h3>경주정보고 취업포털(2차)</h3>
+        <div className={st.content}>
+          <Carousel data={projects['jobportal'].images}></Carousel>
+          <desc>
+            <div>
+              <label>Skills</label>
+              <span className={st.flexwrap}>
+                {projects['jobportal'].skills.map((skill, i) => (
+                  <em key={`temp_jobportal_${skill}_${i}`}>{skill}</em>
+                ))}
+              </span>
+            </div>
+
+            <div>
+              <label>Period</label>
+              <span>2023.11 ~ 2024.02</span>
+            </div>
+            <div>
+              <label>Desc</label>
+              <span>
+                <b>경주정보고 학생들의 취업을 돕기 위한 웹사이트</b>입니다.
+              </span>
+            </div>
+            <div>
+              <label>Site</label>
+              <a href="https://internet-job.kr/" target="_blank">
+                internet-job.kr
+              </a>
+            </div>
+            <div>
+              <label>Detail</label>
+              <div>
+                <p>사람인/잡코리아 취업정보 API 연동</p>
+                <p>필터링 조건 추가 및 삭제 기능 구현</p>
+                <p>사이드바 및 최근페이지 기록</p>
+                <p>기업의 교내공고 생성 및 학생 지원 관리</p>
+                <p>취업지원 프로그램 수강 신청 및 관리</p>
+                <p>로그인 방식별 페이지 구성 상이</p>
               </div>
             </div>
           </desc>
